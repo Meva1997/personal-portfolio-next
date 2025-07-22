44c02de4b63db922d8a5c258f96ebad14835cea0
Alex Medina <alexmedina@192.168.1.238>
2025-07-22 14:03:36 -0600

    added a contact form component

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,12 +1,25 @@
+import ContactForm from "@/components/ContactForm";
 import Footer from "@/components/Footer";
 import MainHeader from "@/components/MainHeader";
-import React from "react";
 
 export default function ContactPage() {
   return (
     <>
       <MainHeader />
-      <main className="min-h-dvh"></main>
+      <main className="flex flex-col items-center justify-center min-h-dvh">
+        <section className="max-w-4xl px-5 mx-auto mb-8 text-center">
+          <h1 className="text-3xl font-bold text-center text-orange-500">
+            Send Me a Message to Get in Touch
+          </h1>
+          <p className="max-w-2xl mx-auto mt-4 text-center text-gray-300">
+            I would love to hear from you! Whether you have a question, want to
+            collaborate, or you want to hire me for a project, feel free to
+            reach out using the form below. I&apos;ll get back to you as soon as
+            possible.
+          </p>
+        </section>
+        <ContactForm />
+      </main>
       <Footer />
     </>
   );
---
+++ components/ContactForm.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { useForm, ValidationError } from "@formspree/react";
+
+export default function ContactForm() {
+  const [state, handleSubmit] = useForm("mqalpqwo");
+  if (state.succeeded) {
+    return (
+      <div className="p-4 text-center text-green-800 bg-green-100 rounded">
+        <p>
+          Thank you! Your message has been sent successfully. I will get back to
+          you soon.
+        </p>
+      </div>
+    );
+  }
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="w-full max-w-lg px-8 pt-6 pb-8 mx-auto mb-4 bg-gray-900 rounded shadow-md"
+    >
+      <h2 className="mb-4 text-2xl font-bold text-center text-orange-500">
+        Form Submission
+      </h2>
+      <div className="mb-4">
+        <label
+          htmlFor="email"
+          className="block mb-2 text-white text-whitefont-semibold"
+        >
+          Email
+        </label>
+        <input
+          id="email"
+          type="email"
+          name="email"
+          placeholder="Your email address"
+          required
+          className="w-full px-3 py-2 border rounded shadow appearance-none text-whitefocus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="message" className="block mb-2 text-whitefont-semibold">
+          Message
+        </label>
+        <textarea
+          id="message"
+          name="message"
+          placeholder="Your message..."
+          required
+          rows={5}
+          className="w-full px-3 py-2 border rounded shadow appearance-none text-whiteresize-none focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
+      </div>
+      <button
+        type="submit"
+        disabled={state.submitting}
+        className="w-full px-4 py-2 font-bold text-white transition bg-blue-500 rounded cursor-pointer hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400"
+      >
+        Send
+      </button>
+    </form>
+  );
+}
