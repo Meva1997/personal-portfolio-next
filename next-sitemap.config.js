06971c4eb6cbdd3d4a6ca35184abba245a46eca7
Alex Medina <alexmedval2@gmail.com>
2025-07-25 09:40:11 -0600

    fix sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://mevadev.com",
+  siteUrl: "https://mevadev.com",
   generateRobotsTxt: true, // (optional)
   outDir: "./public", // Output directory for the sitemap files
   // ...other options
 };
+
+// process.env.SITE_URL ||
