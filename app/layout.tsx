9e460298056b82453bf815f32df9d5a2a3bd03dc
Alex Medina <alexmedval2@gmail.com>
2025-07-28 09:19:39 -0600

    added favicon.png to public

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,6 +63,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/favicon.png" sizes="32x32" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
